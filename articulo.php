a5a14ce485d3f5decbf9d5b2ab7dcb3ebbf91bf3
ComfenalcoCursos2021 <educacioncontinua04@unc.edu.co>
2022-03-14 21:44:55 -0500

    Informacion Terminado

--- articulo.php
+++ articulo.php
@@ -1,3 +1,19 @@
 <?php
     $plantilla->articuloImagen = (string) '<img src="img/bg.jpg" class="bg">';
+    $plantilla->expedicion = (string) '
+    <div class="content">
+        <img src="img/name.png" class="movieTitle">
+        <h4>
+            <span>Elon Reeve Musk. </span>
+            <span>2018. </span>
+            <span>50 Años. </span>
+            <span>Pretoria (Sudáfrica). </span>
+        </h4>
+        <p>El 5 de mayo, SpaceX lanzó y aterrizó con éxito su nave espacial reutilizable, la Starship SN15. Este fue un logro histórico para la nave construida que se convertiría en la primera nave espacial interplanetaria totalmente reutilizable ocupada por humanos. Pero la conversación pública sobre el fundador y consejero delegado de SpaceX, Elon Musk, ha sido una mezcla de ideas, con muchas celebridades e intelectuales que critican la riqueza de Musk y su aspiración de ir a Marte y finalmente colonizar el espacio.</p>
+        <div class="buttons">
+            <a href="#"><ion-icon name="play-outline"></ion-icon> Ver</a>
+            <a href="#"><ion-icon name="add-outline"></ion-icon> Lista</a>
+        </div>
+    </div>
+    ';
 ?>
--- main.js
+++ main.js
@@ -1,10 +1,11 @@
+// https://comfenalcocursos2022.000webhostapp.com/Ejercicio_Maquetacion_PHP_28_02_2022/api.php
 addEventListener("DOMContentLoaded", async(e)=>{
-    let peticion = await fetch("api.php");
+    let peticion = await fetch("https://comfenalcocursos2022.000webhostapp.com/Ejercicio_Maquetacion_PHP_28_02_2022/api.php");
     let json = await peticion.json();
     console.log(json);
     document.querySelector(".logo").insertAdjacentHTML("beforeend", json.imgen);
     document.querySelector(".navigation").insertAdjacentHTML("beforeend", json.menu);
     document.querySelector(".search").insertAdjacentHTML("beforeend", json.buscar);
     document.querySelector(".banner").insertAdjacentHTML("beforeend", json.articuloImagen);
-    
+    document.querySelector(".banner").insertAdjacentHTML("beforeend", json.expedicion);
 })
